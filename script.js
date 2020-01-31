21a36d2698de602841ff9b43214798e33eed38e2
Birk Wertwein <BirkWertwein@Birks-MacBook-Air.local>
2020-01-31 14:49:09 +0100

    function that creates etiquettes and added table to HTML

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-// Hej Vanessa
-// Hej Birk 
+$(document).ready(function() {
+    $.getJSON('dataBas.json', function(response) {
+        console.log(response) // resp = object
+        console.log(response.products[0].productName)
+        for(let i = 0; i < response.products.length; i++) {
+            const etiquetteHolder = $('#etiquette-holder')
+            console.log(etiquetteHolder)
+            etiquetteHolder.append(`
+                <div>
+                    <img src="${response.products[i].img}">
+                    <h3>${response.products[i].productName}</h3>
+                    <p>${response.products[i].price}</p>
+                    <button id="decrease${i}">-</button>
+                    <input id="${i}">
+                    <button class="" id="increase${i}">+</button>
+                    <br>
+                    <button class="addBtn" id="add${i}">Lägg till</button>
+                </div`)
+        }
+        let addBtns = $('.addBtn')
+        console.log(addBtns)
+        addBtns.click(function(){
+            
+            alert(this.id)
+        })
 
+    }) 
+})
