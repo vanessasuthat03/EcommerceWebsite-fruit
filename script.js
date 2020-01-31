d8afd35cf4c66377e96d5193332b88105c7c1d28
Vanessa Suthat <vanessasuthat@Vanessas-MacBook-Pro-2.local>
2020-01-31 15:24:45 +0100

    har lagt till css

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $.getJSON('dataBas.json', function(response) {
+    $.getJSON("dataBas.json", function(response) {
         console.log(response) // resp = object
         console.log(response.products[0].productName)
-        for(let i = 0; i < response.products.length; i++) {
-            const etiquetteHolder = $('#etiquette-holder')
+        for (let i = 0; i < response.products.length; i++) {
+            const etiquetteHolder = $("#etiquette-holder")
             console.log(etiquetteHolder)
             etiquetteHolder.append(`
                 <div>
@@ -12,17 +12,17 @@ $(document).ready(function() {
                     <p>${response.products[i].price}</p>
                     <button id="decrease${i}">-</button>
                     <input id="${i}">
+                    
                     <button class="" id="increase${i}">+</button>
                     <br>
                     <button class="addBtn" id="add${i}">Lägg till</button>
                 </div`)
+            console.log(etiquetteHolder)
         }
-        let addBtns = $('.addBtn')
+        let addBtns = $(".addBtn")
         console.log(addBtns)
-        addBtns.click(function(){
-            
-            alert(this.id)
+        addBtns.click(function() {
+            console.log(this.id)
         })
-
-    }) 
+    })
 })
