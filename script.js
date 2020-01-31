200df70a6eab609cf649041425ef112483aea567
Birk Wertwein <BirkWertwein@Birks-MacBook-Air.local>
2020-01-31 18:55:46 +0100

    sry, råkade paja js-filer, men nu är den tillbaka

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
     $.getJSON("dataBas.json", function(response) {
         // console.log(response) // resp = object
-        // console.log(response.products[0].productName)
         for (let i = 0; i < response.products.length; i++) {
             const etiquetteHolder = $("#etiquette-holder")
-            // console.log(etiquetteHolder)
             etiquetteHolder.append(`
                 <div>
                     <img src="${response.products[i].img}">
@@ -17,19 +15,17 @@ $(document).ready(function() {
                     <br>
                     <button class="addBtn" id="add${i}">Lägg till</button>
                 </div`)
-            console.log(etiquetteHolder)
         }
-        
         let addBtns = $('.addBtn')
-        console.log(addBtns)
-        //console.log($('#add0'))
-        //console.log(document.querySelector('#add0'))
+
         addBtns.click(function(){
-            const value = $('#' + this.id)    // hämtar ett Element i "jQuery-format" för att kunna köra metoden siblings()
-            .siblings('input')  // hämtar alla input-element i en "konstig array" med prototypen object
-            [0] // därför kan vi ta index 0 och få första (och i det här fallet det enda) input-elementet i vanligt format
-            .value  // sen kan vi ta value på det
-            console.log(value)
+            let inputValue = $("#" + this.id).siblings("input")[0].value
+            console.log(inputValue)
+            let price = $("#" + this.id).siblings("p")[0].textContent
+            console.log(price)
+            let name = $("#" + this.id).siblings("h3")[0].textContent
+            console.log(name)
         })
     })
 })
+            
