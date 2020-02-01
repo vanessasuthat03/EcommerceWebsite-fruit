a1a134acc58a33ea793e4713dbd071b0b4dd9445
AngieAraya <angiearaya@hotmail.com>
2020-02-01 18:11:52 +0100

    copied the code from github to avoid conflicts

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
+    localStorage.clear()
     $.getJSON("dataBas.json", function(response) {
-        console.log(response) // resp = object
-        console.log(response.products[0].productName)
+
+        // 1. Loopa ut alla etiketter med respektive produkt, hämtade från json-filen (vår response)
+
         for (let i = 0; i < response.products.length; i++) {
             const etiquetteHolder = $("#etiquette-holder")
-            console.log(etiquetteHolder)
-            etiquetteHolder.append(`
-                <div>
+            etiquetteHolder.append(
+                `<div>
                     <img src="${response.products[i].img}">
                     <h3>${response.products[i].productName}</h3>
                     <p>${response.products[i].price}</p>
@@ -17,18 +18,44 @@ $(document).ready(function() {
                     <br>
                     <button class="addBtn" id="add${i}">Lägg till</button>
                 </div`)
-            console.log(etiquetteHolder)
         }
-        
+
+        // 2. Lägg till eventlistener på alla "Lägg till"-knappar för att skicka data till localStorage
+
         let addBtns = $('.addBtn')
-        console.log(addBtns)
-        //console.log($('#add0'))
-        //console.log(document.querySelector('#add0'))
+
         addBtns.click(function(){
-            $('#' + this.id)    // hämtar ett Element i "jQuery-format" för att kunna köra metoden siblings()
-            .siblings('input')  // hämtar alla input-element i en "konstig array" med prototypen object
-            [0] // därför kan vi ta index 0 och få första (och i det här fallet det enda) input-elementet i vanligt format
-            .value  // sen kan vi ta value på det
+            addToCart(this)
+            
+            // const duplicate = cartArr.find(function(element) {
+            //     return element.product === newProduct // om frukten redan finns i varukorgen så sparas det objektet i duplicate-variabeln
+            // })
         })
+
+    function addToCart(addBtn) {
+
+        // Cache:a antal, produkt och pris
+
+        let newQuantity = $(addBtn).siblings("input").val()
+        let newProduct = $(addBtn).siblings("h3").text()
+        let newPrice = $(addBtn).siblings("p").text()
+        let cartArr = []
+
+        if(localStorage.getItem('cartArr') !== null) { // om cartArr redan finns i localStorage
+            cartArr = JSON.parse(localStorage.getItem('cartArr')) // hämta nuvarande localStorage
+        }
+        cartArr.push({quantity: newQuantity, product: newProduct, price: newPrice}) // lägg in ett objekt med info om tillägget (i slutet av arrayen)
+        localStorage.setItem('cartArr', JSON.stringify(cartArr)) // skicka arrayen till localStorage
+
+        // och lägg till i vår table-tag
+
+        $('table').append(
+            `<tr>
+                <td>${newQuantity}</td>
+                <td>${newProduct}</td>
+                <td>${newPrice}</td>
+                <td><button id="dltBtn">Delete</td>
+            </tr>`)
+    }
     })
 })
