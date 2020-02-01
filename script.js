153e4ed3169c0e20b9059e3b00b069d53f8494b2
ItchyPitchy <birk.wertwein@gmail.com>
2020-02-01 17:24:40 +0100

    tog bort onödiga rader och la till kommentarer

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     localStorage.clear()
     $.getJSON("dataBas.json", function(response) {
-        // console.log(response) // resp = object
+
+        // 1. Loopa ut alla etiketter med respektive produkt, hämtade från json-filen (vår response)
+
         for (let i = 0; i < response.products.length; i++) {
             const etiquetteHolder = $("#etiquette-holder")
-            etiquetteHolder.append(`
-                <div>
+            etiquetteHolder.append(
+                `<div>
                     <img src="${response.products[i].img}">
                     <h3>${response.products[i].productName}</h3>
                     <p>${response.products[i].price}</p>
@@ -17,40 +19,39 @@ $(document).ready(function() {
                     <button class="addBtn" id="add${i}">Lägg till</button>
                 </div`)
         }
+
+        // 2. Lägg till eventlistener på alla "Lägg till"-knappar för att skicka data till localStorage
+
         let addBtns = $('.addBtn')
 
         addBtns.click(function(){
-            let quantity = $(this).siblings("input").val()
-            let price = $(this).siblings("p").text()
-            let product = $(this).siblings("h3").text()
+
+            // Cache:a antal, produkt och pris
+
+            let newQuantity = $(this).siblings("input").val()
+            let newProduct = $(this).siblings("h3").text()
+            let newPrice = $(this).siblings("p").text()
             let cartArr = []
-            if(localStorage.getItem('cartArr') === null) {
-                cartArr.push({quantity: quantity, product: product, price: price})
-                localStorage.setItem('cartArr', JSON.stringify(cartArr))
-                $('table').append(
-                    `<tr>
-                        <td>${JSON.parse(localStorage.getItem('cartArr'))[0].quantity}</td>
-                        <td>${JSON.parse(localStorage.getItem('cartArr'))[0].product}</td>
-                        <td>${JSON.parse(localStorage.getItem('cartArr'))[0].price}</td>
-                        <td><button id="dltBtn">Delete</td>
-                    </tr>`)
-            } else {
-                cartArr = JSON.parse(localStorage.getItem('cartArr'))
-                const duplicate = cartArr.find(function(element) {
-                    return element.product === product
-                })
-                console.log(duplicate)
-                cartArr.push({quantity: quantity, product: product, price: price})
-                localStorage.setItem('cartArr', JSON.stringify(cartArr))
-                let lastElementIndexInArray = cartArr.length - 1
-                $('table').append(
-                    `<tr>
-                        <td>${JSON.parse(localStorage.getItem('cartArr'))[lastElementIndexInArray].quantity}</td>
-                        <td>${JSON.parse(localStorage.getItem('cartArr'))[lastElementIndexInArray].product}</td>
-                        <td>${JSON.parse(localStorage.getItem('cartArr'))[lastElementIndexInArray].price}</td>
-                        <td><button id="dltBtn">Delete</td>
-                    </tr>`)
+
+            if(localStorage.getItem('cartArr') !== null) { // om cartArr redan finns i localStorage
+                cartArr = JSON.parse(localStorage.getItem('cartArr')) // hämta nuvarande localStorage
             }
+            cartArr.push({quantity: newQuantity, product: newProduct, price: newPrice}) // lägg in ett objekt med info om tillägget (i slutet av arrayen)
+            localStorage.setItem('cartArr', JSON.stringify(cartArr)) // skicka arrayen till localStorage
+
+            // och lägg till i vår table-tag
+
+            $('table').append(
+                `<tr>
+                    <td>${newQuantity}</td>
+                    <td>${newProduct}</td>
+                    <td>${newPrice}</td>
+                    <td><button id="dltBtn">Delete</td>
+                </tr>`)
+            
+            // const duplicate = cartArr.find(function(element) {
+            //     return element.product === newProduct // om frukten redan finns i varukorgen så sparas det objektet i duplicate-variabeln
+            // })
         })
     })
 })
